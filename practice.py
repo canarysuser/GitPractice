3fcb0ec8102dd5424724fd47ef080e9016fbda73
Deepali <abc@example.com>
2023-07-17 16:54:08 +0530

    Deepali

---
+++ practice.py
@@ -0,0 +1,10 @@
+# 1. Write a Python script to collect input from the user. The script should count the number of words in the list.
+
+
+
+# Q1
+thelist=input("Enter the sentence in the List: ")
+thelist = thelist.split(' ')
+print("Sentence is: ",thelist)
+print("Total words in the string: ", len(thelist))
+
