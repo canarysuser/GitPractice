13667b87e950499421e8d6ddea734cfeeb441f82
RohitKumar <rohit.kumar167@edgeverve.com>
2023-07-18 16:19:27 +0530

    deleted

--- question2.py
+++
@@ -1,9 +0,0 @@
-s=input("Enter input string : ")
-freq={}
-for x in s:
-    if x in freq:
-       freq[x]=freq[x]+1
-    else:
-       freq[x]=1
-print("Frequency of each letter in the string : ")
-print(freq)
