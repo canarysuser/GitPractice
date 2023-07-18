94afb211035d0a6855a02384c44b89777557b1f9
Naveen Pandiyan <naveen.pandiyan@edgeverve.com>
2023-07-18 15:05:37 +0530

    mithra branch added

--- Q2.py
+++ Q2.py
@@ -1,12 +1,5 @@
-'''2. Write a script to get input line from the user and count the occurences of each letter in the line. '''
-
-string = input("Enter a string: ")
-count_dict = {}
-
-for char in string:
-    if char in count_dict:
-        count_dict[char] += 1
-    else:
-        count_dict[char] = 1
-
-print(count_dict)
+'''Write a script to get input line from the user and count the occurences of each letter in the line. 
+'''
+name = input("enter the user input: ")
+n = [i for i in name]
+print(n)
