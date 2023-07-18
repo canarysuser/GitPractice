d0b62b0073dddc83e8bada66dcfe3fcf02104b83
Naveen Pandiyan <naveen.pandiyan@edgeverve.com>
2023-07-18 12:43:29 +0530

    answering the qn

--- q1.py
+++ q1.py
@@ -1,4 +1,7 @@
-''' Write a Python script to collect input from the user. The script should count the number of words in the list.'''
-name = input("enter the user input: ")
-n = name.split()
-print(n)
+'''1. Write a Python script to collect input from the user. 
+The script should count the number of words in the list.'''
+
+
+script = input("Enter your script : ")
+print(len(script.split()))
+
--- q2.py
+++ q2.py
@@ -1,5 +1,12 @@
-'''Write a script to get input line from the user and count the occurences of each letter in the line. 
-'''
-name = input("enter the user input: ")
-n = [i for i in name]
-print(n)
+'''2. Write a script to get input line from the user and count the occurences of each letter in the line. '''
+
+string = input("Enter a string: ")
+count_dict = {}
+
+for char in string:
+    if char in count_dict:
+        count_dict[char] += 1
+    else:
+        count_dict[char] = 1
+
+print(count_dict)
